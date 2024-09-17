8e57359de6200b39683b7f1a52bf567f2fe27ba7
Overseas Studies <107618449+PrasadJ07@users.noreply.github.com>
2024-09-17 14:17:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 // script.js
 
+const data = {
+    universities: [
+        {
+            name: "Harvard University",
+            programs: [
+                "Computer Science",
+                "Data Analytics"
+            ]
+        },
+        {
+            name: "Stanford University",
+            programs: [
+                "Computer Science",
+                "Artificial Intelligence"
+            ]
+        },
+        {
+            name: "MIT",
+            programs: [
+                "Computer Science",
+                "Data Analytics",
+                "Business Administration"
+            ]
+        },
+        {
+            name: "University of California, Berkeley",
+            programs: [
+                "Computer Science",
+                "Business Administration"
+            ]
+        },
+        {
+            name: "University of Cambridge",
+            programs: [
+                "Artificial Intelligence",
+                "Business Administration"
+            ]
+        }
+    ]
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('search-button');
     const searchBar = document.getElementById('search-bar');
